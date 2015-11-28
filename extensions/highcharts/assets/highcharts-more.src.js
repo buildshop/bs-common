f8eae8c3b91031a65be0620f47673f2a75dcde58
andrtechno <andrew.panix@gmail.com>
2015-11-28 19:05:23 +0200

    Update Highcharts to 4.1.9

--- extensions/highcharts/assets/highcharts-more.src.js
+++ extensions/highcharts/assets/highcharts-more.src.js
@@ -2,7 +2,7 @@
 // @compilation_level SIMPLE_OPTIMIZATIONS
 
 /**
- * @license Highcharts JS v4.1.7 (2015-06-26)
+ * @license Highcharts JS v4.1.9 (2015-10-07)
  *
  * (c) 2009-2014 Torstein Honsi
  *
@@ -78,7 +78,9 @@ extend(Pane.prototype, {
 				config.color = config.backgroundColor; // due to naming in plotBands
 				firstAxis.options.plotBands.unshift(config);
 				axisUserOptions.plotBands = axisUserOptions.plotBands || []; // #3176
-				axisUserOptions.plotBands.unshift(config);
+				if (axisUserOptions.plotBands !== firstAxis.options.plotBands) {
+					axisUserOptions.plotBands.unshift(config);
+				}
 			});
 		}
 	},
@@ -686,6 +688,7 @@ defaultPlotOptions.arearange = merge(defaultPlotOptions.area, {
 seriesTypes.arearange = extendClass(seriesTypes.area, {
 	type: 'arearange',
 	pointArrayMap: ['low', 'high'],
+	dataLabelCollections: ['dataLabel', 'dataLabelUpper'],
 	toYData: function (point) {
 		return [point.low, point.high];
 	},
@@ -828,6 +831,7 @@ seriesTypes.arearange = extendClass(seriesTypes.area, {
 			seriesProto = Series.prototype,
 			dataLabelOptions = this.options.dataLabels,
 			align = dataLabelOptions.align,
+			inside = dataLabelOptions.inside,
 			point,
 			up,
 			inverted = this.chart.inverted;
@@ -839,7 +843,7 @@ seriesTypes.arearange = extendClass(seriesTypes.area, {
 			while (i--) {
 				point = data[i];
 				if (point) {
-					up = point.plotHigh > point.plotLow;
+					up = inside ? point.plotHigh < point.plotLow : point.plotHigh > point.plotLow;
 					
 					// Set preliminary values
 					point.y = point.high;
@@ -873,7 +877,7 @@ seriesTypes.arearange = extendClass(seriesTypes.area, {
 			while (i--) {
 				point = data[i];
 				if (point) {
-					up = point.plotHigh > point.plotLow;
+					up = inside ? point.plotHigh < point.plotLow : point.plotHigh > point.plotLow;
 					
 					// Move the generated labels from step 1, and reassign the original data labels
 					point.dataLabelUpper = point.dataLabel;
@@ -989,6 +993,7 @@ seriesTypes.areasplinerange = extendClass(seriesTypes.arearange, {
 		directTouch: true,
 		trackerGroups: ['group', 'dataLabelsGroup'],
 		drawGraph: noop,
+		crispCol: colProto.crispCol,
 		pointAttrToOptions: colProto.pointAttrToOptions,
 		drawPoints: colProto.drawPoints,
 		drawTracker: colProto.drawTracker,
@@ -1346,7 +1351,8 @@ seriesTypes.boxplot = extendClass(seriesTypes.column, {
 			shapeArgs,
 			color,
 			doQuartiles = series.doQuartiles !== false, // error bar inherits this series type but doesn't do quartiles
-			whiskerLength = parseInt(series.options.whiskerLength, 10) / 100;
+			pointWiskerLength,
+			whiskerLength = series.options.whiskerLength;
 
 
 		each(points, function (point) {
@@ -1431,21 +1437,22 @@ seriesTypes.boxplot = extendClass(seriesTypes.column, {
 					crispCorr = (whiskersAttr['stroke-width'] % 2) / 2;
 					highPlot = highPlot + crispCorr;
 					lowPlot = lowPlot + crispCorr;
+					pointWiskerLength = (/%$/).test(whiskerLength) ? halfWidth * parseFloat(whiskerLength) / 100 : whiskerLength / 2;
 					whiskersPath = [
 						// High whisker
 						'M',
-						crispX - halfWidth * whiskerLength, 
+						crispX - pointWiskerLength, 
 						highPlot,
 						'L',
-						crispX + halfWidth * whiskerLength, 
+						crispX + pointWiskerLength, 
 						highPlot,
 						
 						// Low whisker
 						'M',
-						crispX - halfWidth * whiskerLength, 
+						crispX - pointWiskerLength, 
 						lowPlot,
 						'L',
-						crispX + halfWidth * whiskerLength, 
+						crispX + pointWiskerLength, 
 						lowPlot
 					];
 				}
@@ -1852,6 +1859,7 @@ defaultPlotOptions.bubble = merge(defaultPlotOptions.scatter, {
 	maxSize: '20%',
 	// negativeColor: null,
 	// sizeBy: 'area'
+	softThreshold: false,
 	states: {
 		hover: {
 			halo: {
@@ -1931,19 +1939,41 @@ seriesTypes.bubble = extendClass(seriesTypes.scatter, {
 			pos,
 			zData = this.zData,
 			radii = [],
-			sizeByArea = this.options.sizeBy !== 'width',
-			zRange;
-		
+			options = this.options,
+			sizeByArea = options.sizeBy !== 'width',
+			zThreshold = options.zThreshold,
+			zRange = zMax - zMin,
+			value,
+			radius;
+
 		// Set the shape type and arguments to be picked up in drawPoints
 		for (i = 0, len = zData.length; i < len; i++) {
-			zRange = zMax - zMin;
-			pos = zRange > 0 ? // relative size, a number between 0 and 1
-				(zData[i] - zMin) / (zMax - zMin) : 
-				0.5;
-			if (sizeByArea && pos >= 0) {
-				pos = Math.sqrt(pos);
+
+			value = zData[i];
+
+			// When sizing by threshold, the absolute value of z determines the size
+			// of the bubble.
+			if (options.sizeByAbsoluteValue) {
+				value = Math.abs(value - zThreshold);
+				zMax = Math.max(zMax - zThreshold, Math.abs(zMin - zThreshold));
+				zMin = 0;
 			}
-			radii.push(math.ceil(minSize + pos * (maxSize - minSize)) / 2);
+
+			if (value === null) {
+				radius = null;
+			// Issue #4419 - if value is less than zMin, push a radius that's always smaller than the minimum size
+			} else if (value < zMin) {
+				radius = minSize / 2 - 1;
+			} else {
+				// Relative size, a number between 0 and 1
+				pos = zRange > 0 ? (value - zMin) / zRange : 0.5; 
+
+				if (sizeByArea && pos >= 0) {
+					pos = Math.sqrt(pos);
+				}
+				radius = math.ceil(minSize + pos * (maxSize - minSize)) / 2;
+			}
+			radii.push(radius);
 		}
 		this.radii = radii;
 	},
@@ -1996,7 +2026,7 @@ seriesTypes.bubble = extendClass(seriesTypes.scatter, {
 			point = data[i];
 			radius = radii ? radii[i] : 0; // #1737
 			
-			if (radius >= this.minPxSize / 2) {
+			if (typeof radius === 'number' && radius >= this.minPxSize / 2) {
 				// Shape arguments
 				point.shapeType = 'circle';
 				point.shapeArgs = {
@@ -2012,7 +2042,7 @@ seriesTypes.bubble = extendClass(seriesTypes.scatter, {
 					width: 2 * radius,
 					height: 2 * radius
 				};
-			} else { // below zThreshold
+			} else { // below zThreshold or z = null
 				point.shapeArgs = point.plotY = point.dlBox = UNDEFINED; // #1691
 			}
 		}
@@ -2093,6 +2123,7 @@ Axis.prototype.beforePadding = function () {
 					
 				});
 				series.minPxSize = extremes.minSize;
+				series.maxPxSize = extremes.maxSize;
 				
 				// Find the min and max Z
 				zData = series.zData;
@@ -2117,7 +2148,7 @@ Axis.prototype.beforePadding = function () {
 			radius;
 
 		if (isXAxis) {
-			series.getRadii(zMin, zMax, extremes.minSize, extremes.maxSize);
+			series.getRadii(zMin, zMax, series.minPxSize, series.maxPxSize);
 		}
 		
 		if (range > 0) {
@@ -2131,11 +2162,15 @@ Axis.prototype.beforePadding = function () {
 		}
 	});
 	
-	if (activeSeries.length && range > 0 && pick(this.options.min, this.userMin) === UNDEFINED && pick(this.options.max, this.userMax) === UNDEFINED) {
+
+	if (activeSeries.length && range > 0 && !this.isLog) {
 		pxMax -= axisLength;
 		transA *= (axisLength + pxMin - pxMax) / axisLength;
-		this.min += pxMin / transA;
-		this.max += pxMax / transA;
+		each([['min', 'userMin', pxMin], ['max', 'userMax', pxMax]], function (keys) {
+			if (pick(axis.options[keys[0]], axis[keys[1]]) === UNDEFINED) {
+				axis[keys[0]] += keys[2] / transA; 
+			}
+		});
 	}
 };
 
